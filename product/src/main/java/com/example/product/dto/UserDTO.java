d2458f8a07cfb248a3f1ab5848675e15a1dbc9b1
Kavindi487 <kavindividusari@gmail.com>
2025-11-16 22:00:53 +0530

    Add Lombok and JPA annotations to UserDTO
    UserDTO now uses Lombok annotations for boilerplate code reduction and JPA annotations for entity mapping. Updated pom.xml to specify Lombok version and scope.

--- product/src/main/java/com/example/product/dto/UserDTO.java
+++ product/src/main/java/com/example/product/dto/UserDTO.java
@@ -1,4 +1,13 @@
 package com.example.product.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+
 public class UserDTO {
+    @Id
+    private int id;
+    private String name;
 }
