dec2df88a26ce257e4ad59ea0c916cc0b9f9bfe0
Kavindi487 <kavindividusari@gmail.com>
2025-11-17 23:51:00 +0530

    Add getAllUsers method to UserService
    Introduced getAllUsers method to fetch all users and map them to UserDTOs. Added necessary imports, annotations, and dependencies for UserRepo and ModelMapper.

--- product/src/main/java/com/example/product/service/UserService.java
+++ product/src/main/java/com/example/product/service/UserService.java
@@ -1,4 +1,25 @@
 package com.example.product.service;
 
+import com.example.product.dto.UserDTO;
+import com.example.product.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.internal.bytebuddy.description.method.MethodDescription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public List<UserDTO> getAllUsers(){
+        List<User> users = userRepo.findAll();
+        return modelMapper.map(userList,new TypeToken<List<UserDTO><(){}.getType());
+    }
 }
