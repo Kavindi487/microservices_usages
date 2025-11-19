e9b192e51c527db915ceaf8bcddf2b9285266244
Kavindi487 <kavindividusari@gmail.com>
2025-11-19 20:25:53 +0530

    Annotate UserDTO as JPA entity and add fields
    Added JPA @Entity annotation and Lombok annotations to UserDTO. Introduced 'id' and 'name' fields with @Id for persistence support.

--- order/src/main/java/com/example/order/dto/UserDTO.java
+++ order/src/main/java/com/example/order/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package com.example.order.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class UserDTO {
+    @Id
+    private int id;
+    private String name;
 }
