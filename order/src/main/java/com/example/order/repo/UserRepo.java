2d304493917cc670cad61ee21df8498468e5be08
Kavindi487 <kavindividusari@gmail.com>
2025-11-19 15:42:05 +0530

    Add basic User entity, repository, and service
    Introduces User model, DTO, repository, service, and controller classes to support user management. Adds Lombok and ModelMapper dependencies to pom.xml for code simplification and object mapping.

---
+++ order/src/main/java/com/example/order/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.order.repo;
+
+import com.example.order.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+}
---
+++ order/src/main/java/com/example/order/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.order.service;
+
+import com.example.order.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+}
