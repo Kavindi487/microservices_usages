7e7be41d7cccc7ad99188a1103afd07034ffe895
Kavindi487 <kavindividusari@gmail.com>
2025-11-19 20:27:19 +0530

    Add CRUD endpoints to UserController
    Introduced REST endpoints for user management: get, add, update, and delete users. Integrated UserService and UserDTO to handle user operations via HTTP requests.

--- order/src/main/java/com/example/order/controller/UserController.java
+++ order/src/main/java/com/example/order/controller/UserController.java
@@ -1,4 +1,39 @@
 package com.example.order.controller;
 
+import com.example.order.dto.UserDTO;
+import com.example.order.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUser")
+    public List<UserDTO> getUsers(){
+
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/addUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUsers(userDTO);
+    }
+
+    @PostMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUsers(userDTO);
+    }
+
+    @PostMapping("/deleteUser")
+    public String deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUsers(userDTO);
+    }
 }
+
