36a6aa5ee2e01b65ef3f1fab0583549160c90f0d
Kavindi487 <kavindividusari@gmail.com>
2025-11-20 10:38:44 +0530

    Add CRUD methods to UserService using ModelMapper
    Introduced methods for retrieving, saving, updating, and deleting users in UserService. Utilizes ModelMapper for DTO and entity conversion, improving separation between service and data layers.

--- order/src/main/java/com/example/order/service/UserService.java
+++ order/src/main/java/com/example/order/service/UserService.java
@@ -1,8 +1,13 @@
 package com.example.order.service;
 
+import com.example.order.dto.UserDTO;
+import com.example.order.model.User;
 import com.example.order.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,4 +16,27 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUsers() {
+        List<User>userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDTO>>(){}.getType());
+    }
+
+    public UserDTO saveUsers(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public UserDTO updateUsers(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public String deleteUsers(UserDTO userDTO) {
+        userRepo.delete(modelMapper.map(userDTO, User.class));
+        return "user deleted";
+    }
 }
