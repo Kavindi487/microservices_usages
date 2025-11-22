df969aa499408a2055d1543a043c01cc8ca5538a
Kavindi487 <kavindividusari@gmail.com>
2025-11-22 19:08:15 +0530

    Implement User CRUD operations and JPA integration
    Added JPA annotations and Lombok to UserDTO, refactored UserRepo to a JpaRepository interface, and implemented CRUD methods in UserService using ModelMapper for DTO-entity conversion. These changes enable basic user management and persistence in the application.

--- inventry/src/main/java/com/example/inventry/repo/UserRepo.java
+++ inventry/src/main/java/com/example/inventry/repo/UserRepo.java
@@ -1,4 +1,10 @@
 package com.example.inventry.repo;
 
-public class UserRepo {
+import com.example.inventry.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+
 }
