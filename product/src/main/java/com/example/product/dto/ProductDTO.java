9a39c3854700e5dbb304d83ae9965c62f9e79ea5
Kavindi487 <kavindividusari@gmail.com>
2025-12-01 14:55:26 +0530

    imported lombok  dependency features

--- product/src/main/java/com/example/product/dto/ProductDTO.java
+++ product/src/main/java/com/example/product/dto/ProductDTO.java
@@ -1,9 +1,13 @@
 package com.example.product.dto;
 
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     private int id;
     private int productId;
