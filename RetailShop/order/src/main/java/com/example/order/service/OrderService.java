123d9221ac93bcd6b1c0c79f49257ff3df166729
Kavindi487 <kavindividusari@gmail.com>
2025-12-07 12:06:49 +0530

    updated

--- RetailShop/order/src/main/java/com/example/order/service/OrderService.java
+++ RetailShop/order/src/main/java/com/example/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.order.service;
 
+import com.example.inventry.dto.InventoryDTO;
 import com.example.order.dto.OrderDTO;
 import com.example.order.model.Orders;
 import com.example.order.repo.OrderRepo;
@@ -9,17 +10,24 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @Service
 @Transactional
 public class OrderService {
+    private final WebClient webClient;
+
     @Autowired
     private OrderRepo orderRepo;
 
     @Autowired
     private ModelMapper modelMapper;
+
+    public OrderService(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     // GET ALL ORDERS
     public List<OrderDTO> getAllOrders() {
@@ -29,6 +37,19 @@ public class OrderService {
 
     // SAVE ORDER
     public OrderDTO saveOrder(OrderDTO orderDTO) {
+        Integer itemId = orderDTO.getItemId();
+
+        try{
+           InventoryDTO inventoryResponse = webClient.get()
+                    .uri(uriBuilder ->uriBuilder.path("/api/v1/orders/{id}").build(itemId))
+                    .retrieve()
+                    .bodyToMono(InventoryDTO.class)
+                    .block();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         orderRepo.save(modelMapper.map(orderDTO, Orders.class));
         return orderDTO;
     }
